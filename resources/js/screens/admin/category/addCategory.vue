a0da9a57995fa03e1c2a71142e0c159dc98c4a40
NIkolya <nikolaidrobnitsa@gmail.com>
2023-10-14 14:33:12 +0300

    add admin category

---
+++ resources/js/screens/admin/category/addCategory.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-container style="background: white;">
+        <v-form ref="form">
+            <v-text-field
+                :counter="35"
+                label="Name"
+                v-model="name"
+            ></v-text-field>
+
+            <v-text-field
+                label="Order"
+                required
+                v-model="order"
+            ></v-text-field>
+
+            <v-row class="mb-2">
+                <font-awesome-icon
+                    icon="camera"
+                    class="white--text mt-4 ml-2"
+                    style="font-size:30px;margin-right:-20px"
+                />
+                <v-file-input label="Image" show-size v-model="image"></v-file-input>
+            </v-row>
+
+            <v-btn color="success" v-on:click="submitForm()" class="mr-4">
+                Submit
+            </v-btn>
+        </v-form>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            name: '',
+            order: null,
+            image: null
+        }
+    },
+    methods: {
+        submitForm(){
+            const FormData = require('form-data');
+            const formData = new FormData();
+            formData.append('name', this.name);
+            formData.append('order', this.order);
+            formData.append('image', this.image);
+            axios.post('api/add/category',formData).then(response =>{
+                if (response.status >= 200 && response.status < 300){
+                    this.$router.push('categories')
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style  scoped>
+
+</style>
---
+++ resources/js/screens/admin/category/categories.vue
@@ -0,0 +1,103 @@
+<template>
+    <v-simple-table>
+        <template v-slot:default>
+            <thead>
+            <tr >
+                <th class="text-center">
+                    Image
+                </th>
+                <th class="text-center">
+                    Name
+                </th>
+                <th class="text-center">
+                    Order
+                </th>
+                <th class="text-center">
+                    Edit
+                </th>
+                <th class="text-center">
+
+                    Delete
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+
+            <tr v-for="category in categories" :key="category.id">
+                <td  class="text-center">
+                    <div v-if="category.image">
+                        <img class="pt-2" :src="url+'/categories/'+category.image" style="height: 100px; width: 100px" alt="">
+                    </div>
+                    <div v-else>
+                        <img class="pt-2" src="https://thumb.tildacdn.com/tild6463-3761-4638-a261-653363643361/-/format/webp/IMG_4057.JPG" style="height: 100px; width: 100px" alt="">
+                    </div>
+                </td>
+                <td class="text-center">{{ category.name}}</td>
+                <td class="text-center">{{ category.order}}</td>
+                <td class="text-center">
+                    <v-btn
+                        class="mx-2"
+                        fab
+                        dark
+                        large
+                        color="cyan"
+                        v-on:click="() => $router.push({name: 'editCategory', params: {category}})"
+                    >
+                        <font-awesome-icon icon="pen" />
+                    </v-btn>
+                </td>
+                <td class="text-center">
+                    <v-btn
+                        class="mx-2"
+                        fab
+                        dark
+                        large
+                        color="red"
+                        v-on:click="deleteCategory(category.id)"
+                    >
+                        <font-awesome-icon icon="trash" />
+                    </v-btn>
+                </td>
+            </tr>
+            </tbody>
+        </template>
+    </v-simple-table>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            categories: [],
+            url: window.location.origin
+        }
+    },
+    methods:{
+        getCategories(){
+            axios.get('api/categories')
+                .then(response =>{
+                    if (response.status >= 200 && response.status < 300){
+                        this.categories = response.data.categories;
+                    }
+                })
+        },
+
+        deleteCategory(id){
+            axios.get('api/delete/category/'+id).then(response =>{
+                if (response.status >= 200 && response.status < 300){
+                    alert(response.data.message)
+                    this.getCategories()
+                }
+            });
+        }
+    },
+    mounted() {
+        this.getCategories();
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
