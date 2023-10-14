d2791871ee720b7cb2dac1f6cde1da2b68fe075f
NIkolya <nikolaidrobnitsa@gmail.com>
2023-10-14 14:31:48 +0300

    add home page and login page

---
+++ resources/js/screens/client/HomePage.vue
@@ -0,0 +1,168 @@
+<template>
+    <div >
+
+        <v-parallax src="https://thumb.tildacdn.com/tild6566-3537-4534-b663-303237366161/-/format/webp/_2.jpg">
+            <v-main>
+                <div class="text-center " style="margin-top:13%;  ">
+                    <h1 class=" white--text v-text-field--outlined ">
+                        ОДЕСА —
+                    </h1>
+                    <h1 class=" white--text v-text-field--outlined ">
+                        найкраще місце для яскравого відпочинку в Кам'янському
+                    </h1>
+                </div>
+            </v-main>
+        </v-parallax>
+        <v-container style="background-color: white;margin-top: -10px;" fill-width fluid>
+            <v-layout row>
+                <v-flex xs12 style="font-weight: bold;" class="text-center display-1 ma-10">
+                    ЗНАЙОМСТВО З НАМИ
+                </v-flex>
+
+            </v-layout>
+
+            <v-row style="margin-top:60px">
+                <v-col cols="6">
+                    <v-row>
+                        <v-col cols="2">
+
+                        </v-col>
+                        <v-col cols="10" width>
+                            <v-img width="100%" height="85%" src="https://static.tildacdn.com/tild3432-3935-4163-b239-346130646530/IMG_4256.JPG"></v-img>
+                        </v-col>
+                    </v-row>
+                </v-col>
+                <v-col cols="6" class="black--text" style="align-self:center">
+                    <div class="ml-4" style="width:75%">
+                  <span style="font-size:27px; font-weight: bold;" >
+                    Ласково просимо на територію відпочинку «ОДЕСА»
+                  </span>
+                        <br>
+                        <span>
+                    На вас чекає затишний, комфортний, веселий і водночас релаксуючий відпочинок. До Ваших послуг: величезне озеро, чистий пісочний пляж, на якому розташовані шезлонги та зони під накриттям, альтанки, бари, комфортні номери, можливість зайнятися різними водними видами спорту.
+Також, у нас є карпатські чани та лазня на дровах.
+                  </span>
+                    </div>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col cols="6" class="black--text" style="align-self:center">
+                    <v-row justify="end">
+                        <div class="mb-2 mr-4" style="width:75%">
+                        <span style="font-size:27px; font-weight: bold;">
+                            Сімейний відпочинок на чистому пляжі
+                        </span>
+                            <br>
+                            <span>
+                            Територія відпочинку розташована в курортній частині селища Єлизаветівка на березі Блакитних Озер за 50 км від Дніпра, і за 10 км від Кам'янського.
+Ми приймаємо відпочиваючих цілий рік. Завітайте у гості до нас, та насолоджуйтесь приємним відпочинком. Ви полюбите це місце та захочете сюди повернутися.
+                        </span>
+                        </div>
+                    </v-row>
+                </v-col>
+                <v-col cols="6">
+                    <v-row>
+                        <v-col cols="10">
+                            <v-img width="100%" src="https://static.tildacdn.com/tild6437-6266-4536-a635-633833623661/IMG_5967_1.PNG"></v-img>
+                        </v-col>
+                        <v-col cols="2"></v-col>
+                    </v-row>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col cols="6">
+                    <v-row>
+                        <v-col cols="2"></v-col>
+                        <v-col cols="10">
+                            <v-img width="100%" src="https://static.tildacdn.com/tild6166-6533-4364-a331-396665366638/DJI_089400_05_08_27S.jpg"></v-img>
+                        </v-col>
+                    </v-row>
+                </v-col>
+                <v-col cols="6" class="black--text" style="align-self:center">
+                    <div class="ml-4" style="width:36%">
+                  <span style="font-size:23px" class="mb-2">
+                  Пляж
+                  </span>
+                        <br>
+                        <span>
+                    Найкраще місце для вашого спекотного літнього відпочинку. Чистий пісочний пляж обладнаний: дитячою зоною, пляжними барами, шезлонгами та зонами під накриттям – chill zone на 2х, 3х та 6х осіб.
+Також розташовані душові кабінки, санвузли, роздягальні.
+                  </span>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-container>
+
+        <v-img style="width:100%;" src="https://tesla-cdn.thron.com/delivery/public/image/tesla/2ddb3f38-d056-43cc-8e35-c849e7817ded/bvlatuR/std/2880x1800/MX-Interior-Hero-Desktop"></v-img>
+        <div style="background-color:black;margin-top:-10px">
+            <div fluid class="background" style="display:table">
+                <div style="display:table-cell;vertical-align:bottom;padding-bottom:50px">
+                    <v-row>
+                        <v-col>
+                            <h1 class="white--text font-weight-light text-right">
+                                1020hp
+                            </h1>
+                            <p class="white--text text-right">Peak Power</p>
+                        </v-col>
+                        <v-col>
+                            <h1 class="white--text font-weight-light text-center">
+                                9.9s
+                            </h1>
+                            <p class="white--text text-center">1/4 mile</p>
+                        </v-col>
+                        <v-col>
+                            <h1 class="white--text font-weight-light">
+                                2.5s
+                            </h1>
+                            <p class="white--text">0-60 mph*</p>
+                        </v-col>
+                    </v-row>
+                </div>
+            </div>
+        </div>
+        <div style="background-color: black;margin-top:-10px;">
+            <div class="text-left pt-3">
+                <v-row>
+                    <img style="height:600px" src="https://tesla-cdn.thron.com/delivery/public/image/tesla/3d7892b2-7246-42e5-819a-cc119ede91b7/bvlatuR/std/1440x1080/MX-Specs-Hero-Desktop" />
+                    <v-layout align-center>
+                        <h1 class="white--text">Tesla Model X</h1>
+                        <v-btn style="border-radius:18px;" class="ml-3">Order Now</v-btn>
+                    </v-layout>
+                </v-row>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.promo {
+    background: url('https://thumb.tildacdn.com/tild6566-3537-4534-b663-303237366161/-/format/webp/_2.jpg');
+    background-size: cover;
+    width:100%;
+    height:100vh;
+}
+.promo_2{
+    background: url('https://thumb.tildacdn.com/tild6566-3537-4534-b663-303237366161/-/format/webp/_2.jpg');
+    background-size: cover;
+    width:100%;
+    height:200vh;
+}
+.v-text-field--outlined {
+
+    font-family:monospace;
+    font-size: 40px;
+    text-shadow: 2px 1px 3px black;
+}
+.background {
+    background: url('/assets/tesla-specs.jpeg');
+    background-size:cover;
+    width: 100%;
+    height:70vh;
+}
+</style>
